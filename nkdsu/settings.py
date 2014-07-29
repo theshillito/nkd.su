ca472b2b475185ce5ee7b574341ee07dccec6722
Iain Dawson <iain@colons.co>
2014-07-30 00:01:26 +0900

    Remove workaround for https://code.djangoproject.com/ticket/23070
    The workaround was introduced in
    3f7b0d0fd764174ed88c7924800e68ec3dbcde13 and the bug was fixed as
    django/django#2967.

--- nkdsu/settings.py
+++ nkdsu/settings.py
@@ -30,16 +30,16 @@ EMAIL_HOST_USER = 'nivi@musicfortheblind.co.uk'
 EMAIL_USE_TLS = True
 
 # stuff about when the show is
-from dateutil.relativedelta import SA as sa, relativedelta
+from dateutil.relativedelta import SA, relativedelta
 SHOWTIME = relativedelta(
-    weekday=sa,
+    weekday=SA,
     hour=21,
     minute=0,
     second=0,
     microsecond=0,
 )
 SHOW_END = relativedelta(
-    weekday=sa,
+    weekday=SA,
     hour=23,
     minute=0,
     second=0,
